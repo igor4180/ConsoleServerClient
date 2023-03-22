67d5b5c203ff1cecc833501c30f03a82869d5dff
--- <---.mainry>
2023-03-22 21:09:03 +0300

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+try
+{
+    await socket.ConnectAsync("127.0.0.1", 8888);
+    Console.WriteLine($"Подключение к {socket.RemoteEndPoint} установлено");
+}
+catch (SocketException)
+{
+    Console.WriteLine($"Не удалось установить подключение с {socket.RemoteEndPoint}");
+}
+IPEndPoint point = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 80);
+socket.Connect(point);
+byte[] buffer = new byte[1024];
+int c;
+do
+{
+    c = socket.Receive(buffer);
+    Console.WriteLine(Encoding.UTF8.GetString(buffer));
+}
+while (c > 0);
+
+using var udpClient = new UdpClient(8001);
+var brodcastAddress = IPAddress.Parse("235.5.5.11");
+
+udpClient.JoinMulticastGroup(brodcastAddress);
+Console.WriteLine("Начало прослушивания сообщений");
+while (true)
+{
+    var result = await udpClient.ReceiveAsync();
+    string message = Encoding.UTF8.GetString(result.Buffer);
+    if (message == "END")
+    { 
+
+    Thread.Sleep(7000);
+        break; }
+    Console.WriteLine(message);
+}
+
+udpClient.DropMulticastGroup(brodcastAddress);
+Console.WriteLine("Udp-клиент завершил свою работу");
---
+++ ConsoleServerClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 8888);
+using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+socket.Bind(ipPoint);
+socket.Listen();
+Console.WriteLine("Сервер запущен. Ожидание подключений...");
+// получаем входящее подключение
+using Socket client = await socket.AcceptAsync();
+// получаем адрес клиента
+Console.WriteLine($"Адрес подключенного клиента: {client.RemoteEndPoint}");
+
+var messages = new string[] { "Hello World!", "Hello METANIT.COM", "Hello work", "ENDA" };
+var brodcastAddress = IPAddress.Parse("235.5.5.11");
+using var udpSender = new UdpClient();
+Console.WriteLine("Начало отправки сообщений...");
+// отправляем сообщения
+foreach (var message in messages)
+{
+    Console.WriteLine($"Отправляется сообщение: {message}");
+    byte[] data = Encoding.UTF8.GetBytes(message);
+    await udpSender.SendAsync(data, new IPEndPoint(brodcastAddress, 8001));
+    await Task.Delay(1000);
+}
